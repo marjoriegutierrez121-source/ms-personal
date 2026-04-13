ad6fd6e301055609546f1e13fb2566f4a138035e
@juliobazalar28 <juliobazalar28@gmail.com>
2026-04-13 17:01:15 -0500

    Up last version.

--- src/main/java/pe/inpe/ms_personal/MsPersonalApplication.java
+++ src/main/java/pe/inpe/ms_personal/MsPersonalApplication.java
@@ -2,8 +2,10 @@ package pe.inpe.ms_personal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsPersonalApplication {
 
 	public static void main(String[] args) {
